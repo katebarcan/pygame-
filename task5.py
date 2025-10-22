67397a013c14bb242224911072a3c545587fe08b
katebarcan <ekaterinabarcan49@gmail.com>
2025-10-22 19:43:14 +0400

    Update task5.py

--- task5.py
+++ task5.py
@@ -2,11 +2,13 @@ import pygame as pg
 
 FPS = 60
 WIDTH, HEIGHT = 1000, 600
-CANNON_WIDTH, CANNON_HEIGHT = * 1 / 4, HEIGHT * 1 / 4
+CANNON_WIDTH, CANNON_HEIGHT = WIDTH * 1 / 4, HEIGHT * 1 / 4
 MINT = (230, 254, 212)
 ORANGE = (255, 150, 100)
 BROWN = (87, 20, 8)
 GRAY = (186, 184, 182)
+RED = (245, 17, 17)
+BLUE = (101, 17, 245)
 
 pg.init()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -20,10 +22,19 @@ pg.draw.rect(background, GRAY, (0, 350, WIDTH, 120))
 
 cannon = pg.Surface((CANNON_WIDTH, CANNON_HEIGHT), pg.SRCALPHA)
 cannon.fill((0, 0, 0, 0))
-pg.draw.
+pg.draw.circle(cannon, (*RED, 100), (CANNON_WIDTH / 2, CANNON_HEIGHT / 2), 40)
+x1, y1 = 0, 90
+
+# cannon = pg.Surface((CANNON_WIDTH, CANNON_HEIGHT), pg.SRCALPHA)
+# cannon.fill((0, 0, 0, 0))
+# pg.draw.circle(cannon, (*BLUE, 100), (CANNON_WIDTH / 2, CANNON_HEIGHT / 2), 40)
+# x2, y2 = 0, 340
 
 screen.blit(background, (0, 0))
+screen.blit(cannon, (x1, y1))
+# screen.blit(cannon, (x2, y2))
 pg.display.update()
+
 
 flag_play = True
 while flag_play:
@@ -34,8 +45,13 @@ while flag_play:
             pg.quit()
             flag_play = False
             break
+
     if not flag_play:
             break
 
+    
+
     screen.blit(background, (0, 0))
+    screen.blit(cannon, (x1, y1))
+    # screen.blit(cannon, (x2, y2))
     pg.display.update()
