71fe529929da412185ebe0493a0dda3208e0e235
katebarcan <ekaterinabarcan49@gmail.com>
2025-10-25 18:59:59 +0400

    Update task5.py

--- task5.py
+++ task5.py
@@ -20,23 +20,27 @@ background.fill(BROWN)
 pg.draw.rect(background, GRAY, (0, 100, WIDTH, 120))
 pg.draw.rect(background, GRAY, (0, 350, WIDTH, 120))
 
-cannon = pg.Surface((CANNON_WIDTH, CANNON_HEIGHT), pg.SRCALPHA)
-cannon.fill((0, 0, 0, 0))
-pg.draw.circle(cannon, (*RED, 100), (CANNON_WIDTH / 2, CANNON_HEIGHT / 2), 40)
+cannon1 = pg.Surface((CANNON_WIDTH, CANNON_HEIGHT), pg.SRCALPHA)
+cannon1.fill((0, 0, 0, 0))
+pg.draw.circle(cannon1, (*RED, 100), (CANNON_WIDTH / 2, CANNON_HEIGHT / 2), 40)
 x1, y1 = 0, 90
 
-# cannon = pg.Surface((CANNON_WIDTH, CANNON_HEIGHT), pg.SRCALPHA)
-# cannon.fill((0, 0, 0, 0))
-# pg.draw.circle(cannon, (*BLUE, 100), (CANNON_WIDTH / 2, CANNON_HEIGHT / 2), 40)
-# x2, y2 = 0, 340
+cannon2 = pg.Surface((CANNON_WIDTH, CANNON_HEIGHT), pg.SRCALPHA)
+cannon2.fill((0, 0, 0, 0))
+pg.draw.circle(cannon2, (*BLUE, 100), (CANNON_WIDTH / 2, CANNON_HEIGHT / 2), 20)
+x2, y2 = 0, 340
 
 screen.blit(background, (0, 0))
-screen.blit(cannon, (x1, y1))
-# screen.blit(cannon, (x2, y2))
+screen.blit(cannon1, (x1, y1))
+screen.blit(cannon2, (x2, y2))
 pg.display.update()
 
+speed1 = 5
+speed2 = 7
 
 flag_play = True
+flag1 = 'вправо'
+flag2 = 'вправо'
 while flag_play:
     clock.tick(FPS)
 
@@ -45,13 +49,33 @@ while flag_play:
             pg.quit()
             flag_play = False
             break
-
     if not flag_play:
             break
 
-    
+    if x1 >= WIDTH:
+        flag1 = 'влево'
+    if x1 <= -220:
+        flag1 = 'вправо'
+
+    if flag1 == 'вправо':
+        x1 += speed1
+        y1 = 90
+    if flag1 == 'влево':
+        x1 -= speed1
+        y1 = 340
+
+    if x2 >= WIDTH:
+        flag2 = 'влево'
+    if x2 <= -220:
+        flag2 = 'вправо'
+    if flag2 == 'вправо':
+        x2 += speed2
+        y2 = 90
+    if flag2 == 'влево':
+        x2 -= speed2
+        y2 = 340
 
     screen.blit(background, (0, 0))
-    screen.blit(cannon, (x1, y1))
-    # screen.blit(cannon, (x2, y2))
+    screen.blit(cannon1, (x1, y1))
+    screen.blit(cannon2, (x2, y2))
     pg.display.update()
