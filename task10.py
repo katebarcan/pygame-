ad748e895c7a97cda079f1c73df223eb124c82b2
katebarcan <ekaterinabarcan49@gmail.com>
2025-12-06 19:42:44 +0400

    Update task10.py

--- task10.py
+++ task10.py
@@ -1,19 +1,55 @@
 import pygame as pg
+
 FPS = 60
 WIN_WIDTH, WIN_HEIGHT = 1000, 600
 WHITE, BLACK = (255, 255, 255), (0, 0, 0)
 GREEN_MINT = (225, 247, 228)
 
+class Car:
+    speed = 5
+
+    def __init__(self):
+        self.car_surf = pg.image.load(r"images\car.png").convert_alpha()
+        self.car_rect = self.car_surf.get_rect(center=(WIN_WIDTH / 2, WIN_HEIGHT / 2))
+        self.direction = "up"
+
+    def rotate(self, new_direction):
+        # self.direction = "up", new_direction="left" => +90
+        # self.direction = "up", new_direction="right" => +270
+        # self.direction = "up", new_direction="bottom" => +180
+        #
+
+
+
+    def draw(self, screen):
+        screen.blit(self.car_surf, self.car_rect)
+
+
 pg.init()
 screen = pg.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pg.display.set_caption("Игра")
-pg.display.set_icon(pg.images.load(r"images\icon.ico"))
 clock = pg.time.Clock()
 
-car_surf = pg.image.load(r"images\car_processed.png").convert_alpha()
-car_rect = car_surf.get_rect(center=(WIN_WIDTH / 2, WIN_HEIGHT / 2))
+my_car = Car()
 
 screen.fill(GREEN_MINT)
-screen.blit(car_surf, car_rect)
+my_car.draw(screen)
 pg.display.update()
-pg.time.wait(2 * 1000)
+
+flag_play = True
+while flag_play:
+    clock.tick(FPS)
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            flag_play = False
+            break
+    if not flag_play:
+        break
+
+
+
+    screen.fill(GREEN_MINT)
+    my_car.draw(screen)
+    pg.display.update()
