2e4fe72ea2d6e27c9efe15c724cdc879f3c23456
katebarcan <ekaterinabarcan49@gmail.com>
2026-01-17 19:39:40 +0400

    Update task13.py

--- task13.py
+++ task13.py
@@ -3,9 +3,10 @@ import random
 
 FPS = 60
 WIDTH, HEIGHT = 800, 600
+BLACK = (0, 0, 0)
 
 SHIP_SIZE = (100, 100)
-ASTEROID_SIZE = (100, 100)
+ASTEROID_SIZE = (109, 50)
 FIREBALL_SIZE = (25, 50)
 
 class Ship(pg.sprite.Sprite):
@@ -40,8 +41,21 @@ class Asteroid(pg.sprite.Sprite):
         if self.rect.top > HEIGHT:
             self.kill()
 
+class Fireball(pg.sprite.Sprite):
+    speed = 10
 
+    def __init__(self, x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load(r"images\the fireball.png").convert_alpha()
+        self.image = pg.transform.rotate(self.image, 90)
+        self.image = pg.transform.scale(self.image, FIREBALL_SIZE)
+        self.rect = self.image.get_rect(center=(x, y))
+        self.mask = pg.mask.from_surface(self.image)
 
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.bottom < 0:
+            self.kill()
 
 
 pg.init()
@@ -49,11 +63,23 @@ screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Игра")
 clock = pg.time.Clock()
 
-# здесь происходит создание игровых объектов:
-# ...
+pg.mixer.music.load(r"audio\background_music.mp3")
+pg.mixer.music.play(-1)
 
-# если надо до игрового цикла (=на самом старте игры) отобразить объекты, то отрисовываем их здесь:
-# ...
+crash_sound = pg.mixer.Sound(r"audio\collision_with_an_asteroid.mp3")
+fireball_sound = pg.mixer.Sound(r"audio\fireball_shot.flac")
+
+img = pg.image.load(r"images\background.jpg").convert
+img = pg.transform.scale(img, WIDTH, HEIGHT)
+
+sprites = pg.sprite.Group()
+asteroid = pg.sprite.Group()
+fireball = pg.sprite.Group
+
+player = Ship()
+sprites.add(player)
+
+
 pg.display.update()
 
 
@@ -70,10 +96,7 @@ while flag_play:
     if not flag_play:
         break
 
-    # изменение характеристик объектов:
-    # ...
 
-    # перерисовка экрана:
-    # ...
+
 
     pg.display.update()
